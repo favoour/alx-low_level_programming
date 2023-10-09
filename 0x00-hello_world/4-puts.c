5ffd8aa2145d17e291f614a7855eb4922ef2b230
favoour <favour.fisayo@yahoo.com>
2023-10-09 16:08:42 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,12 @@
 /**
  * Main - Entry point
  *
- * This function prints the given string to the console.
- * It is also the entry point for the program.
- *
  * Return: 0 after printing the string
- *
- * Arguments: None
  */
 int main(void)
 {
     puts("Programming is like building a multilingual puzzle");
-    return 0;
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
+
