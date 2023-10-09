9c8c553de295278792000f729a17033de9ecb28d
favoour <favour.fisayo@yahoo.com>
2023-10-09 09:24:25 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+
 /**
-* main - Entry point
-* 
-* Return: always 0 (success)
-*/}
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+
+        return (0);
+}
+
