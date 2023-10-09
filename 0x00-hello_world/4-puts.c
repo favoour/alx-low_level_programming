5ca9cdaac7a4dc7846b8aaf98bc03094e7d873b1
favoour <favour.fisayo@yahoo.com>
2023-10-09 16:15:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
 /**
  * Main - Entry point
  *
- * Return: 0 after printing the string
+ * This function prints the given string and returns 0.
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
     puts("Programming is like building a multilingual puzzle");
     return 0;
 }
