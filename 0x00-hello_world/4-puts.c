e8e1afce3aa83575729e5d7cf64faf6fafb1a3a8
favoour <favour.fisayo@yahoo.com>
2023-10-09 17:07:39 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
