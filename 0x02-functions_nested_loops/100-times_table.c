da3877635467bb5da2157c63fb26e7ebe9d4420b
favoour <favour.fisayo@yahoo.com>
2023-10-11 17:15:13 -0700

    table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,64 +8,44 @@
 
 void print_times_table(int n)
 {
-	int i, j, s;
-	int max_result, max_digits, temp_max, temp_result;
-	int spaces;
+	int i, j, result;
 
 	if (n < 0 || n > 15)
 	{
 		return;
 	}
-	max_result = n * n;
-	max_digits = 0;
-	temp_max = max_result;
-
-	while (temp_max > 0)
+	else
 	{
-		max_digits++;
-		temp_max /= 10;
-	}
-
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			int result = i * j;
-			int digits = 0;
-
-			temp_result = result;
-
-			while (temp_result > 0)
-			{
-				digits++;
-				temp_result /= 10;
-			}
-			spaces = max_digits - digits;
-
-			for (s = 0; s < spaces; s++)
-			{
-				_putchar(' ');
-			}
-
-			if (j > 0)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
 
-			if (result < 10)
-			{
-				_putchar('0' + result);
-			}
-			else
-			{
-				while (result > 0)
+				result = i * j;
+				if (result <= 99)
 				{
-					_putchar('0' + result % 10);
-					result /= 10;
+					_putchar(' ');
 				}
+				if (result <= 9)
+				{
+					_putchar(' ');
+				}
+				if (result >= 100)
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+				}
+				else if (result <= 99 && result >= 10)
+				{
+					_putchar((result / 10) + '0');
+				}
+				_putchar((result % 10) + '0');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+
 }
